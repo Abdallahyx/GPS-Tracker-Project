1f46f5aba4811b11f06fe04ea972213c7ed06f43
Abdallahyx <99212200+Abdallahyx@users.noreply.github.com>
2023-05-09 16:51:50 +0300

    Add files via upload

--- gps.c
+++ gps.c
@@ -1,19 +1,16 @@
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
-#include <stdlib.h>
-
-#define PI 3.142857 
+#include "gps.h"
 
 
-char const GPS_LOG[] = "$GPRMC,";
+char GPS_LOG[] = "$GPRMC,";
 char GPS[80];
 char GPS_PROCESSED[12][20];
 char * token;
-double lng, lat;
+double curlng, curlat, deslng, deslat;
 const double R = 6371000;
 const double FINAL_lng, FINAL_lat;
 double total_distance;
+char Longitude[12];
+char Latitude[12];
 
 
 double ToDEGREE(double angle){
@@ -30,6 +27,7 @@ void GPSread(){
     char UART_char;
     int count = 0;
     int i = 0;
+
 
     do{
         while(UART_GETinput() != GPS_LOG[count])
@@ -56,30 +54,63 @@ void GPSread(){
 
     if (strcmp(GPS_PROCESSED[1], "A") == 0){
         if (strcmp(GPS_PROCESSED[3], "N") == 0){
-            lat = atof(GPS_PROCESSED[2]);
+            curlat = atof(GPS_PROCESSED[2]);
         }
         else{
-            lat = -atof(GPS_PROCESSED[2]);
+            curlat = -atof(GPS_PROCESSED[2]);
         }
         if (strcmp(GPS_PROCESSED[5], "E") == 0){
-            lng = atof(GPS_PROCESSED[4]);
+            curlng = atof(GPS_PROCESSED[4]);
         }
         else{   
-            lng = -atof(GPS_PROCESSED[4]);
+            curlng = -atof(GPS_PROCESSED[4]);
         }
-        
+
     }
 
 }
 
-double Calc_distance(double currentlong, double currentlat, double destlong, double destlat){
-    double currentLatrad = ToRAD(ToDEGREE(currentlat)); 
-    double destLatrad = ToRAD(ToDEGREE(destlat));
-    double longDiff = ToRAD(ToDEGREE(destlong)-ToDEGREE(currentlong));
-    double latDiff = ToRAD(ToDEGREE(destlat)-ToDEGREE(currentlat));
+double distance(){
+    double currentLatrad = ToRAD(ToDEGREE(curlat)); 
+    double destLatrad = ToRAD(ToDEGREE(deslat));
+    double longDiff = ToRAD(ToDEGREE(deslng)-ToDEGREE(curlng));
+    double latDiff = ToRAD(ToDEGREE(deslat)-ToDEGREE(curlat));
     double a = pow(sin(latDiff/2), 2)+cos(currentLatrad)*cos(destLatrad)*pow(sin(longDiff/2), 2);
     double c = 2 * atan2(sqrt(a), sqrt(1 - a));
     double distance = R*c;
     total_distance += distance; // total distance moved saved in global var
     return distance;
+}
+
+
+void toString(char str[], int num){
+    int i, rem, len = 0, n;
+    n = num;
+    while (n != 0)
+    {
+        len++;
+        n /= 10;
+    }
+    for (i = 0; i < len; i++)
+    {
+        rem = num % 10;
+        num = num / 10;
+        str[len - (i + 1)] = rem + '0';
+    }
+    str[len] = '\0';
+}
+
+
+void DoubleToString(float x, char c[]){
+    int y = (int)x;
+    x=x-y;
+    char tmp[12];
+    tostring(tmp, y);
+    y=(int)(x*100000);
+    char tmp1[5];
+    tostring(tmp1,y);
+    strcat(tmp,".");
+    strcat(tmp,tmp1);
+    strcpy(c,tmp);
+
 }
